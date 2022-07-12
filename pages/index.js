0b7edc832a1e056999c62a8d99c33fce0c25adda
nishaya raj <nishayaraj@gmail.com>
2022-07-12 13:40:20 -0500

    Button text change, seperate ren dering of joke and punline added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [jokeButtonState, setJokeButtonState] = useState('Get a joke');
+
+  const handleClick = () => {
+    if (jokeButtonState === 'Get a joke' || jokeButtonState === 'Get another joke') {
+      getJoke().then((data) => {
+        setJoke(data);
+        setJokeButtonState('Show punch line');
+      });
+    } else if (jokeButtonState === 'Show punch line') {
+      setJokeButtonState('Get another joke');
+    }
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +26,9 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h2>{joke.setup}</h2>
+      {jokeButtonState !== 'Show punch line' && <h2>{joke.delivery}</h2>}
+      <button type="button" onClick={handleClick}>{jokeButtonState}</button>
     </div>
   );
 }
